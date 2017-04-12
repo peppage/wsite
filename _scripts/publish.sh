63296a5c525727f1ca7173c0860f2aa8f9012b6a
Michael Peppler <peppage@gmail.com>
2017-04-12 08:21:21 -0400

    uncomment out committing

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -25,5 +25,5 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-#echo "Updating gh-pages branch"
-#cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
